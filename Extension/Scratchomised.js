cc34254caa229f435d6fb906216786fe27803090
Totchi Lagawi <chatnoir420@gmx.com>
2024-11-06 10:40:24 +0100

    Add working communication between client and server

--- Extension/Scratchomised.js
+++ Extension/Scratchomised.js
@@ -1,41 +1,45 @@
 
-// Classe représentant l'extension
+// Class representing the extension
 class Scratchomised {
     constructor() {
-        const port = 5125;
-        this.socket = new WebSocket("ws://localhost:" + port);
+        this.port = 55125;
+        this.host = "localhost"
+        this._reconnectSocket();
+        this._objects = {}
+
+        this._prefix = "[Scratchomised] - "
     }
 
-    // Donne des informations à Scratch sur l'extension
+    //Give informations to Scratch about the extension
     getInfo() {
         return {
-            // ID de l'extension (unique à chaque extension)
+            // Identifier of the extension (unique)
             id: "scratchomised",
 
-            // Nom de l'extension affiché dans Scratch
+            // Name of the extension, as shown in Scratch
             name: "Scratchomised",
 
-            // Blocs affichés dans Scratch
+            // Blocks shown in Sratch
             blocks: [
                 {
-                    // L'opcode est le nom de la fonction appellée quand le bloc est utilisé
+                    // The opcode is the name of the function called when the block is used
                     opcode: "defineProperty",
 
-                    // Définit le type du bloc
-                    // "command" -> Bloc "classique", qui exécute une action
+                    // Define block type
+                    // "command" -> "Classic" block
                     blockType: "command",
 
-                    // Texte affiché dans Scratch pour le bloc
-                    // Le texte entre crochets représente les arguments
+                    // Text shown in Scratch for the block
+                    // Text between "[" and "]" is arguments
                     text: "Define property [PROPERTY] of [OBJECT] to [VALUE]",
 
-                    // Définit les arguments du bloc
+                    // Define block's arguments
                     arguments: {
                         PROPERTY: {
-                            // L'argument est un texte
+                            // Argument is a text
                             type: "string",
 
-                            // L'argument est un menu nommé "properties"
+                            // And we can choose it from a menu called "properties"
                             menu: "properties"
                         },
                         OBJECT: {
@@ -63,23 +67,40 @@ class Scratchomised {
                             menu: "objects"
                         }
                     }
+                },
+                {
+                    opcode: "connectToHost",
+                    blockType: "command",
+                    text: "Connect to host [HOST] with port [PORT]",
+                    arguments: {
+                        HOST: {
+                            type: "string"
+                        },
+                        PORT: {
+                            type: "number"
+                        }
+                    }
                 }
             ],
 
-            // Définit les menus des blocs de l'extension
+            // Define the extension's menus
             menus: {
 
-                // Menu "properties"
-                // Il s'agit d'un menu statique : il ne peut pas être modifié selon des évènements extérieurs
+                // "Properties" menu
+                // It is a static menu
                 properties: {
-                    // Éléments du menu
+                    // Menu elements
                     items: "getProperties"
                 },
                 objects: {
-                    // Le menu "objects" est un menu dynamique :
-                    // Au lieu des éléments du menu, un nom de fonction est donné
-                    // Cette fonction sera appellé chaque fois que le menu est ouvert
-                    // Elle va retourner les éléments du menu
+                    // The menu "object" is a dynamic menu
+                    // Instead of giving an array, the name of a function is given
+                    // This function will be called each time the menu is opened
+                    // It will return an array containing the menu's elements
+                    //
+                    // /!\ WARNING /!\
+                    // Due to a strange behaviour of the TurboWarp VM, dynamic menus only works
+                    // in unsandboxed extensions
                     items: "getObjects"
                 }
             }
@@ -87,50 +108,98 @@ class Scratchomised {
     }
 
     defineProperty(args) {
-        try {
-            this.socket.send(JSON.stringify({
-                action: "define_property",
-                object: "Light",
-                property: "Powered",
-                value: "True"
-            }))
-        } catch (e) {
-            console.log("Error while sending data");
-        }
-        console.log("Defining property : " + args);
+        this._send("define_property", {
+            object: args.OBJECT,
+            property: args.PROPERTY,
+            value: args.VALUE
+        });
     }
 
     getProperty(args) {
         try {
-            this.socket.send(JSON.stringify({
-                action: "get_property",
-                object: "Light",
-                property: "Powered"
-            }))
-        } catch (e) {
-            console.log("Error while sending data")
+            return this._objects[args.OBJECT][args.PROPERTY]
+
+        } catch (error) {
+            console.error(this._prefix + "error while getting property " + args.PROPERTY + " of object " + args.OBJECT);
         }
-        console.log("Getting property : " + args);
     }
 
     getObjects() {
-        try {
-            this.socket.send(JSON.stringify({
-                action: "get_objects"
-            }))
-        } catch (e) {
-            console.log("Error while sending data")
+        var keys = Object.keys(this._objects);
+        if (keys.length == 0) {
+            return ["(Empty)"]
+        } else {
+            return keys
         }
-        console.log("Getting objects...");
-        return ["Hello", "World!"];
     }
 
     getProperties() {
-        return ["A", "Proper", "Property!"]
+        var final_properties = []
+        const array = Object.values(this._objects);
+        for (var index = 0; index < array.length; index++) {
+            const properties = Object.keys(array[index]);
+            for (var index2 = 0; index2 < properties.length; index2++) {
+                console.debug(this._prefix + "trying to add property : " + properties[index2]);
+                if (!final_properties.includes(properties[index2])) {
+                    final_properties.push(properties[index2])
+                }
+            }
+        }
+        if (final_properties.length == 0) {
+            return ["(Empty)"]
+        } else {
+            return final_properties
+        }
     }
 
-    _sendData(data) {
+    connectToHost(args) {
+        this.host = args.HOST
+        this.port = args.PORT
+        this._reconnectSocket();
+    }
 
+    // Function to send data to the server
+    _send(action, args = {}) {
+        try {
+            this.socket.send(JSON.stringify({
+                action: action,
+                args: args
+            }))
+        } catch (error) {
+            console.error(this._prefix + "error while sending data : \n" + error);
+        }
+    }
+
+    // Reconnect the socket (in case it failed to connect, or the server disconnected)
+    _reconnectSocket() {
+        this.socket = new WebSocket("ws://" + this.host + ":" + this.port);
+        this.socket.addEventListener("open", function () {
+            this._send("get_objects")
+        }.bind(this));
+        this.socket.addEventListener("message", this._handleIncomingData.bind(this));
+    }
+
+    // Handle data sent by the server
+    _handleIncomingData(event) {
+        try {
+            var message = JSON.parse(event.data)
+        } catch (error) {
+            console.error(this._prefix + "unable to parse message : \n" + event.data + "\n" + this._prefix + " got error : " + error);
+            return
+        }
+
+        switch (message.action) {
+            case "set_objects": {
+                if (!message.args.objects) {
+                    console.error(this._prefix + "missing argument for action set_objects : objects");
+                }
+                this._objects = message.args.objects
+                break
+            }
+            default: {
+                console.log(this._prefix + "unknow action : " + message.action)
+            }
+        }
     }
 }
 
--- Testing Server/main.py
+++ Testing Server/main.py
@@ -3,26 +3,42 @@ import asyncio
 import json
 from websockets.server import serve
 
-async def main():
+objects = {
+    "wall": {
+        "colour": "white"
+    },
+    "lamp": {
+        "powered": True
+    }
+}
 
+async def main():
     print("Server started")
-    async with serve(handle_connection, "localhost", 5125):
+    async with serve(handle_connection, "localhost", 55125):
         await asyncio.get_running_loop().create_future()
     print("Goodbye!")
 
 async def handle_connection(socket):
     async for message in socket:
         instruction = json.loads(message)
+        print("Got message!")
         match instruction["action"]:
             case "get_objects":
                 print("Getting objects...")
+                await send(socket, "set_objects", {
+                    "objects": objects
+                })
             case "define_property":
-                print(instruction)
-                print("Defining property " + instruction["property"] + " of object " + instruction["object"] + " to " + instruction["value"] + "...")
+                print("Defining property " + instruction["args"]["property"] + " of object " + instruction["args"]["object"] + " to " + instruction["args"]["value"] + "...")
             case "get_property":
-                print(instruction)
-                print("Getting property " + instruction["property"] + " of object " + instruction["object"] + "...")
+                print("Getting property " + instruction["args"]["property"] + " of object " + instruction["args"]["object"] + "...")
             case _:
                 print("Unknown action : " + instruction["action"])
+
+async def send(socket, action, args = {}):
+    await socket.send(json.dumps({
+        "action": action,
+        "args": args
+    }))
 
 asyncio.run(main())

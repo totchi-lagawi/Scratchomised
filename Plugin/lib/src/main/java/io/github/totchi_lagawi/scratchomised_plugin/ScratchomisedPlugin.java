3b6ef4fdb6194ec337fecfe11bf75b22eeb806db
Totchi Lagawi <chatnoir420@gmx.com>
2024-11-14 21:00:42 +0100

    First plugin files

---
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/ScratchomisedPlugin.java
@@ -0,0 +1,11 @@
+package io.github.totchi_lagawi.scratchomised_plugin;
+
+import com.eteks.sweethome3d.plugin.Plugin;
+import com.eteks.sweethome3d.plugin.PluginAction;
+
+class ScratchomisedPlugin extends Plugin {
+    @Override
+    public PluginAction[] getActions() {
+        return null;
+    }
+}

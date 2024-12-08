d76b1a10fd08d25bacf0edaaad02c1f3b6cd3f83
Totchi Lagawi <chatnoir420@gmx.com>
2024-12-08 22:59:12 +0100

    First version of the plugin's translation manager

---
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/LanguageManager.java
@@ -0,0 +1,46 @@
+package io.github.totchi_lagawi.scratchomised_plugin;
+
+public class LanguageManager {
+    public static String getString(String id, String lang) {
+        if (lang == null) {
+            lang = getLang();
+        }
+
+        switch (id) {
+            case "plugin.menu": {
+                return "Scratchomised";
+            }
+
+            case "export.name": {
+                return "Export...";
+            }
+
+            case "server.name.start": {
+                return "Start server";
+            }
+
+            case "server.name.stop": {
+                return "Stop server";
+            }
+
+            case "error.error": {
+                return "Error";
+            }
+
+            case "error.server.socket_bind": {
+                return "Error while binding server socket.";
+            }
+
+            case "error.server.null_server": {
+                return "The server couldn't be instanciated, please rerun SweetHome3D.";
+            }
+        }
+
+        return null;
+    }
+
+    public static String getLang() {
+        // TODO
+        return null;
+    }
+}

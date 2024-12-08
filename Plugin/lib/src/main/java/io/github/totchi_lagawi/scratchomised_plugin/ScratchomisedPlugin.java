1828b54537e8b92ff13e9f2dab544164142f1a57
Totchi Lagawi <chatnoir420@gmx.com>
2024-12-08 23:00:28 +0100

    Little modification to update actions

--- Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/ScratchomisedPlugin.java
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/ScratchomisedPlugin.java
@@ -6,6 +6,9 @@ import com.eteks.sweethome3d.plugin.PluginAction;
 public class ScratchomisedPlugin extends Plugin {
     @Override
     public PluginAction[] getActions() {
-        return new PluginAction[] {new ScratchomisedPluginActionManageServer()};
+        return new PluginAction[] {
+            new PluginActionManageServer(),
+            new PluginActionExportDatas()
+        };
     }
 }

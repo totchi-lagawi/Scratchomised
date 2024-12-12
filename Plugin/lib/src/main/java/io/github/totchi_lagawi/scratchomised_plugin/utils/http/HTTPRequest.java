c253b6711b21dc3e74859f1fe1eb43310f69a611
Totchi Lagawi <chatnoir420@gmx.com>
2024-12-12 21:23:26 +0100

    Add support for body to the HTTPRequest class, and allow getting the raw request

--- Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/utils/http/HTTPRequest.java
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/utils/http/HTTPRequest.java
@@ -1,18 +1,22 @@
 package io.github.totchi_lagawi.scratchomised_plugin.utils.http;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HTTPRequest {
     private HTTPMethod _method;
     private HTTPVersion _version;
     private String _location;
     private HashMap<String, String> _headers;
+    private String _body;
 
-    public HTTPRequest(HTTPMethod method, HTTPVersion version, String location, HashMap<String, String> headers) {
+    public HTTPRequest(HTTPMethod method, HTTPVersion version, String location, HashMap<String, String> headers,
+            String body) {
         this._method = method;
         this._version = version;
         this._location = location;
         this._headers = headers;
+        this._body = body;
     }
 
     public HTTPMethod getMethod() {
@@ -24,11 +28,14 @@ public class HTTPRequest {
     }
 
     /**
-     * Get the HTTP version as a <code>double</code>
+     * Get the HTTP version of the request as a <code>double</code>
+     * 
+     * @throws IllegalStateException when the version of the request is unknown,
+     *                               probably null
      * 
      * @return the double representing the version
      */
-    public double getVersionDouble() {
+    public double getVersionDouble() throws IllegalStateException {
         switch (this._version) {
             case ZERO_POINT_NINE:
                 return 0.9;
@@ -53,5 +60,18 @@ public class HTTPRequest {
 
     public HashMap<String, String> getHeaders() {
         return this._headers;
+    }
+
+    public String getBody() {
+        return this._body;
+    }
+
+    public String getRawRequest() {
+        StringBuilder request = new StringBuilder();
+        request.append(this._method + " " + this._location + " " + this.getVersionDouble() + "\n");
+        for (Map.Entry<String, String> entry : this._headers.entrySet()) {
+            request.append(entry.getKey() + ": " + entry.getValue() + "\n");
+        }
+        return request.toString();
     }
 }

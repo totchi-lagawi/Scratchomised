6902cd881d75c4381b901bbbca9be52ce2bb5407
Totchi Lagawi <chatnoir420@gmx.com>
2025-01-29 11:36:10 +0100

    Delete the WebSocketException, not necessary since the WebServer is being remade

--- Plugin/lib/src/main/java/io/github/totchi_lagawi/websocket_server/WebSocketException.java
+++
@@ -1,8 +0,0 @@
-package io.github.totchi_lagawi.websocket_server;
-
-public class WebSocketException extends Exception {
-    public WebSocketException(String message) {
-        super(message);
-    }
-
-}

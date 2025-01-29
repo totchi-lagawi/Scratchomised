ce581790a8a4d54855c192eec98551152bd0778c
Totchi Lagawi <chatnoir420@gmx.com>
2025-01-29 22:02:49 +0100

    Add basic functionnalities except run() to the connection handler

--- Plugin/lib/src/main/java/io/github/totchi_lagawi/websocket_server/ConnectionHandler.java
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/websocket_server/ConnectionHandler.java
@@ -1,7 +1,35 @@
 package io.github.totchi_lagawi.websocket_server;
 
-public class ConnectionHandler implements Runnable {
-    public void run() {
+import java.net.Socket;
 
+public class ConnectionHandler implements Runnable {
+    // The parent server, to call methods like onOpen() or onClose()
+    private Server _server;
+    // The connection to the client
+    private ServerConnection _connection;
+
+    /**
+     * Instanciate the connection handler
+     * 
+     * @param socket the socket to the client
+     */
+    public ConnectionHandler(Socket socket) {
+        this._connection = new ServerConnection(socket);
+    }
+
+    /**
+     * Run the connection handler
+     */
+    public void run() {
+        // TODO
+        // Implement hanshaking, then receiving messages and passing them to the
+        // appropriate methods
+    }
+
+    /**
+     * Stop the connection
+     */
+    public void stop() {
+        this._connection.close();
     }
 }

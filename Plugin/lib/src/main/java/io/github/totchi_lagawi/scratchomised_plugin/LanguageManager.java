8460d7b61647ca95dd7a197e81faef175ab5e7a4
Totchi Lagawi <chatnoir420@gmx.com>
2025-02-19 18:50:48 +0100

    Remove debug prints in LanguageManager and Translation

--- Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/LanguageManager.java
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/LanguageManager.java
@@ -41,11 +41,9 @@ public class LanguageManager implements PropertyChangeListener {
     }
 
     public void loadTranslation(String lang, boolean replace) {
-        System.out.println("Loading translation : " + lang);
         // Check if it is really useful to load the translation
         if (replace = false) {
             if (this._translations.containsKey(lang)) {
-                System.out.println("Translation already present, skipping");
                 return;
             }
         }
@@ -55,14 +53,12 @@ public class LanguageManager implements PropertyChangeListener {
 
         // Ensure that the translation file actually exists
         if (file_stream == null) {
-            System.out.println("Huh? Translation file not found");
             throw new NoSuchElementException("The translation file for " + lang + " was not found");
         }
 
         Yaml yaml = new Yaml();
         Translation translation = (Translation) yaml.loadAs(file_stream, Translation.class);
         this._translations.put(lang, translation);
-        System.out.println("Now we have the following translations : " + this._translations.keySet());
     }
 
     public void propertyChange(PropertyChangeEvent event) {
--- Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/Translation.java
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/Translation.java
@@ -1,6 +1,5 @@
 package io.github.totchi_lagawi.scratchomised_plugin;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 /**
@@ -41,16 +40,11 @@ public class Translation {
      * @throws NoSuchElementException if the request translation was not found
      */
     public String getTranslation(String id) throws NoSuchElementException, IllegalAccessException {
-        System.out.println("Getting the translation for " + id);
-        System.out.println("Resulting array : " + Arrays.toString(id.split("\\.")));
         try {
             Object object = this;
-            System.out.println("Base object : " + object);
             for (String field : id.split("\\.")) {
                 object = object.getClass().getDeclaredField(field).get(object);
-                System.out.println("Object is now : " + object + " of type " + object.getClass().getName());
             }
-            System.out.println("We now have the translation " + (String) object);
             return (String) object;
         } catch (NoSuchFieldException ex) {
             throw new NoSuchElementException("The requested translation (" + id + ") was not found");

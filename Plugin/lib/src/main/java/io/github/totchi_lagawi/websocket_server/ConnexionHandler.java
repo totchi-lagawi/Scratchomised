ac3a49035d330da14a1b070db6e653a0639326e1
Totchi Lagawi <chatnoir420@gmx.com>
2025-03-25 08:31:39 +0100

    Send back HTTP error to the client

--- Plugin/lib/src/main/java/io/github/totchi_lagawi/websocket_server/ConnexionHandler.java
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/websocket_server/ConnexionHandler.java
@@ -1,6 +1,12 @@
 package io.github.totchi_lagawi.websocket_server;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+
+import io.github.totchi_lagawi.http_utils.HTTPException;
+import io.github.totchi_lagawi.http_utils.HTTPResponse;
+import io.github.totchi_lagawi.http_utils.HTTPVersion;
 
 public class ConnexionHandler implements Runnable {
     // The parent server, to call methods like onOpen() or onClose()
@@ -20,18 +26,34 @@ public class ConnexionHandler implements Runnable {
 
     /**
      * Run the connexion handler
+     * 
+     * @throws HTTPException
      */
     public void run() {
-        this._server.onOpen(_connexion);
-        // TODO
-        // Implement hanshaking, then receiving messages and passing them to the
-        // appropriate methods
+        try {
+            this._connexion.performHandshake();
+        } catch (HTTPException ex) {
+            HashMap<String, String> response_headers = new HashMap<>();
+            response_headers.put("Connection", "Close");
+            try {
+                this._connexion
+                        .sendHTTPResponse(
+                                new HTTPResponse(HTTPVersion.ONE_POINT_ONE, ex.getStatusCode(), response_headers,
+                                        "<h1>Error " + String.valueOf(ex.getStatusCode() + "</h1>")));
+                this._connexion.close();
+            } catch (IOException ex2) {
+                ex2.printStackTrace();
+            }
+            this._server.onError(_connexion, ex);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
      * Stop the connexion
      */
-    public void stop() {
+    public void close() throws IOException {
         this._connexion.close();
     }
 }

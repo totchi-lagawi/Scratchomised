f829f4be38b5253b98db00944fab5988a30cf2c9
Totchi Lagawi <chatnoir420@gmx.com>
2025-05-06 21:21:24 +0200

    Add constructors to ScratchomisedRequest

--- Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/ScratchomisedRequest.java
+++ Plugin/lib/src/main/java/io/github/totchi_lagawi/scratchomised_plugin/ScratchomisedRequest.java
@@ -4,5 +4,20 @@ import java.util.Hashtable;
 
 public class ScratchomisedRequest {
     public String action;
-    public Hashtable<String, String> args;
+    public Hashtable<String, Object> args;
+
+    public ScratchomisedRequest(String action, Hashtable<String, Object> args) {
+        this.action = action;
+        this.args = args;
+    }
+
+    public ScratchomisedRequest(String action) {
+        this.action = action;
+        this.args = new Hashtable<>();
+    }
+
+    public ScratchomisedRequest() {
+        this.action = "";
+        this.args = new Hashtable<>();
+    }
 }
